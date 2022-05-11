694c755423713d70d4944a31d8dc29cea5fae74e
Vitor Viana <masterviana@gmail.com>
2022-05-11 13:36:15 +0100

    Update signDocumentsCollections.sol

--- contracts/signDocumentsCollections.sol
+++ contracts/signDocumentsCollections.sol
@@ -1,82 +1,79 @@
-// contracts/GameItems.sol
-// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract SimpleAccessControl {
 
-    address internal _owner;
-
+    mapping (address => uint8) internal _owners;
 
     modifier isOwner() {
-        require(msg.sender == _owner, "ERR_NOT_OWNER");
+        require(_owners[msg.sender] > 0, "ERR_NOT_OWNER");
         _;
     }
-    /*
-    constructor(address payable ownerAdress) {
-        _owner = ownerAdress; 
-    }
-    */
+    
+    //constructor(address[] payable ownerAdress) {
+    //    _owner = ownerAdress; 
+    //}
+
     constructor() {
-        _owner = msg.sender; 
+        _owners[msg.sender] = 10;
     }
 
-    function transferOwnership(address payable newOwner) public isOwner {
+    function addOwnership(address payable newOwner) public isOwner {
         require(newOwner != address(0), "ERR_ZERO_ADDR");
-        require(newOwner != _owner, "ERR_IS_OWNER");
-        _owner =   newOwner;
+       _owners[newOwner] = 10;
+    }
+    
+    function removeOwnership(address payable newOwner) public isOwner {
+        require(newOwner != address(0), "ERR_ZERO_ADDR");
+       _owners[newOwner] = 0;
     }
 }
 
 contract eSignDocsCollection  is ERC1155, SimpleAccessControl 
 {
     
-    uint256 private _id = 0;
+    uint256 private _id = 1;
     string public name;
     string private _contractURI;
 
-   mapping(uint256 => DocumentInfo) private _idsToDocument;
-   mapping(bytes => uint256) private _hashDocToNftId;
-   //mapping(address => uint256[]) private  _signersToIds;
+    mapping(uint256 => DocumentInfo) private _idsToDocument;
+    mapping(bytes => uint256) private _hashDocToNftId;
+    //mapping(address => uint256[]) private  _signersToIds;
 
     struct DocumentInfo 
     {
         address[] signers;
-        bytes32 envelopeId;
+        bytes envelopeId;
         bytes documentHash;
         uint256 nftId;
     }
 
-
-    constructor() public ERC1155("https://demo.api.wallid/api/v1/documentsMetadata/detail/{id}")
+    constructor() public ERC1155("https://demo.api.wallid.io/api/v1/metadata/detail/{id}")
     {
-        _contractURI = "https://demo.api.wallid/api/v1/documentsMetadata/detail/";
-        name = "Signed Document Info";
-       
+        _contractURI = "https://demo.api.wallid.io/api/v1/metadata/";
+        name = "WalliD PoS NTFs";
+    }
+
+    function lastIndex() public view returns (uint256) {
+        return _id;
     }
 
     // get nft info based on id
-    function getSignersInfo (uint256 id ) public view returns (address[] memory, bytes32, bytes memory) {
+    function getSignersInfo (uint256 id ) public view returns (address[] memory, bytes memory, bytes memory) {
         return (_idsToDocument[id].signers, _idsToDocument[id].envelopeId, _idsToDocument[id].documentHash);    
     }
 
      // get nft info based on document hash
-     function getSignersInfo (bytes memory docHash ) public view returns (address[] memory, bytes32, bytes memory) 
+     function getSignersInfo (bytes memory docHash ) public view returns (address[] memory, bytes memory, bytes memory) 
      {
         uint256 _id = _hashDocToNftId[docHash];
         return (_idsToDocument[_id].signers, _idsToDocument[_id].envelopeId, _idsToDocument[_id].documentHash);    
      }
 
-
-    function mintDocument( address[] memory signers, bytes32 envelopeId, bytes memory docHash   ) public returns (bool)
+    function mintDocument( address[] memory signers, bytes memory envelopeId, bytes memory docHash   ) public isOwner  returns (uint256) 
     {
-        _id = _id +1; 
-       
-        //DocumentInfo storage newDocInfo = DocumentInfo(signers, envelopeId ,  docHash , newId );
-
-        // fooStruct myStruct = fooStruct({foo:1, fighter:2});
-
+        
         // store document struct
         _idsToDocument[_id].signers = signers;
         _idsToDocument[_id].envelopeId = envelopeId;
@@ -85,23 +82,14 @@ contract eSignDocsCollection  is ERC1155, SimpleAccessControl
         // hash document to id
         _hashDocToNftId[docHash] = _id;
      
-
         for (uint i = 0; i<signers.length; i++) 
         {
             _mint(signers[i], _id, 1, "");
         }
 
-        return true;
+        _id = _id + 1;
+        return _id;
     }
-
-    /*
-    function mint(uint256 total) public returns (bool)
-    {
-        require( (total >= 1 && total <= 10) , 'NFT_MINT: GREATER THEN 1 LESS THEN 10' );
-        _mint(msg.sender, _id++, (total) * (1 ether), "");
-        return true;
-    }
-    */
 
     /* Metadata information */
     /* set uri of itens */
